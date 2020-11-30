17eca87ad45ff60a130b9059d80527d5b631d611
adamevs90 <65515026+adamevs90@users.noreply.github.com>
2020-11-30 21:05:58 +0000

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -2,7 +2,7 @@
 // Copyright (c) 2014-2018, The Monero Project
 // Copyright (c) 2018, The TurtleCoin Developers
 // Copyright (c) 2018, The BLOC Developers
-//
+// Copyright (c) 2020, The BLOC Money
 // Please see the included LICENSE file for more information.
 
 #pragma once
